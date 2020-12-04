7521c098b0750268d5db64fbd85c9d29aacb83d0
Michael Jeanson <mjeanson@efficios.com>
2020-12-04 17:04:02 -0500

    cds_hlist_entry -> lttng_ust_hlist_entry
    Change-Id: I09552d36cd65d694dc6f578191c0eacb3e806bc8

--- include/lttng/urcu/hlist.h
+++ include/lttng/urcu/hlist.h
@@ -40,7 +40,7 @@ void LTTNG_UST_INIT_HLIST_HEAD(struct lttng_ust_hlist_head *ptr)
 	{ .next = NULL }
 
 /* Get typed element from list at a given position. */
-#define cds_hlist_entry(ptr, type, member) \
+#define lttng_ust_hlist_entry(ptr, type, member) \
 	((type *) ((char *) (ptr) - (unsigned long) (&((type *) 0)->member)))
 
 /* Add new element at the head of the list. */
@@ -80,30 +80,30 @@ void cds_hlist_del(struct lttng_ust_hlist_node *elem)
  */
 #define cds_hlist_for_each_entry(entry, pos, head, member) \
 	for (pos = (head)->next, \
-			entry = cds_hlist_entry(pos, __typeof__(*entry), member); \
+			entry = lttng_ust_hlist_entry(pos, __typeof__(*entry), member); \
 		pos != NULL; \
 		pos = pos->next, \
-			entry = cds_hlist_entry(pos, __typeof__(*entry), member))
+			entry = lttng_ust_hlist_entry(pos, __typeof__(*entry), member))
 
 #define cds_hlist_for_each_entry_safe(entry, pos, p, head, member) \
 	for (pos = (head)->next, \
-			entry = cds_hlist_entry(pos, __typeof__(*entry), member); \
+			entry = lttng_ust_hlist_entry(pos, __typeof__(*entry), member); \
 		(pos != NULL) && (p = pos->next, 1); \
 		pos = p, \
-			entry = cds_hlist_entry(pos, __typeof__(*entry), member))
+			entry = lttng_ust_hlist_entry(pos, __typeof__(*entry), member))
 
 #define cds_hlist_for_each_entry_2(entry, head, member) \
 	for (entry = ((head)->next == NULL ? NULL \
-			: cds_hlist_entry((head)->next, __typeof__(*entry), member)); \
+			: lttng_ust_hlist_entry((head)->next, __typeof__(*entry), member)); \
 		entry != NULL; \
 		entry = (entry->member.next == NULL ? NULL \
-			: cds_hlist_entry(entry->member.next, __typeof__(*entry), member)))
+			: lttng_ust_hlist_entry(entry->member.next, __typeof__(*entry), member)))
 
 #define cds_hlist_for_each_entry_safe_2(entry, e, head, member) \
 	for (entry = ((head)->next == NULL ? NULL \
-			: cds_hlist_entry((head)->next, __typeof__(*entry), member)); \
+			: lttng_ust_hlist_entry((head)->next, __typeof__(*entry), member)); \
 		(entry != NULL) && (e = (entry->member.next == NULL ? NULL \
-					: cds_hlist_entry(entry->member.next, \
+					: lttng_ust_hlist_entry(entry->member.next, \
 						__typeof__(*entry), member)), 1); \
 		entry = e)
 
