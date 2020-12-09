2f6f7af8fa5addd2fd8d9bb18ad11fe8a1a6d0a3
Michael Jeanson <mjeanson@efficios.com>
2020-12-09 14:47:09 -0500

    MEMOP_OUT -> LTTNG_UST_MEMOP_OUT
    Change-Id: I1099c65d4662ad9322c03e1420b986040afec655

--- include/lttng/urcu/uatomic/s390.h
+++ include/lttng/urcu/uatomic/s390.h
@@ -49,13 +49,13 @@ extern "C" {
 
 #ifdef LTTNG_UST_COMPILER_HAVE_SHORT_MEM_OPERAND
 
-#define MEMOP_OUT(addr)	"=Q" (*(addr))
+#define LTTNG_UST_MEMOP_OUT(addr)	"=Q" (*(addr))
 #define MEMOP_IN(addr)	"Q" (*(addr))
 #define MEMOP_REF(op)	#op		/* op refer to MEMOP_IN operand */
 
 #else /* !LTTNG_UST_COMPILER_HAVE_SHORT_MEM_OPERAND */
 
-#define MEMOP_OUT(addr)	"=m" (*(addr))
+#define LTTNG_UST_MEMOP_OUT(addr)	"=m" (*(addr))
 #define MEMOP_IN(addr)	"a" (addr), "m" (*(addr))
 #define MEMOP_REF(op)	"0(" #op ")"	/* op refer to MEMOP_IN operand */
 
@@ -79,7 +79,7 @@ unsigned long _uatomic_exchange(volatile void *addr, unsigned long val, int len)
 		__asm__ __volatile__(
 			"0:	cs %0,%2," MEMOP_REF(%3) "\n"
 			"	brc 4,0b\n"
-			: "=&r" (old_val), MEMOP_OUT (__hp(addr))
+			: "=&r" (old_val), LTTNG_UST_MEMOP_OUT (__hp(addr))
 			: "r" (val), MEMOP_IN (__hp(addr))
 			: "memory", "cc");
 		return old_val;
@@ -92,7 +92,7 @@ unsigned long _uatomic_exchange(volatile void *addr, unsigned long val, int len)
 		__asm__ __volatile__(
 			"0:	csg %0,%2," MEMOP_REF(%3) "\n"
 			"	brc 4,0b\n"
-			: "=&r" (old_val), MEMOP_OUT (__hp(addr))
+			: "=&r" (old_val), LTTNG_UST_MEMOP_OUT (__hp(addr))
 			: "r" (val), MEMOP_IN (__hp(addr))
 			: "memory", "cc");
 		return old_val;
@@ -123,7 +123,7 @@ unsigned long _uatomic_cmpxchg(void *addr, unsigned long old,
 
 		__asm__ __volatile__(
 			"	cs %0,%2," MEMOP_REF(%3) "\n"
-			: "+r" (old_val), MEMOP_OUT (__hp(addr))
+			: "+r" (old_val), LTTNG_UST_MEMOP_OUT (__hp(addr))
 			: "r" (_new), MEMOP_IN (__hp(addr))
 			: "memory", "cc");
 		return old_val;
@@ -133,7 +133,7 @@ unsigned long _uatomic_cmpxchg(void *addr, unsigned long old,
 	{
 		__asm__ __volatile__(
 			"	csg %0,%2," MEMOP_REF(%3) "\n"
-			: "+r" (old), MEMOP_OUT (__hp(addr))
+			: "+r" (old), LTTNG_UST_MEMOP_OUT (__hp(addr))
 			: "r" (_new), MEMOP_IN (__hp(addr))
 			: "memory", "cc");
 		return old;
