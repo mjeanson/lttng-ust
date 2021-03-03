d5fe4d25fbd44a9c39c37d1bcdc7950ff22d2d09
Michael Jeanson <mjeanson@efficios.com>
2021-03-03 13:00:31 -0500

    wip: extern ustfork symbols
    Change-Id: Ia02f798ce318b9b6e5c1e1573d0fa8af3cad1d07

--- include/lttng/ust.h
+++ include/lttng/ust.h
@@ -13,19 +13,25 @@
 extern "C" {
 #endif
 
-extern void ust_before_fork(sigset_t *save_sigset);
-extern void ust_after_fork_parent(sigset_t *restore_sigset);
-extern void ust_after_fork_child(sigset_t *restore_sigset);
-extern void ust_after_setns(void);
-extern void ust_after_unshare(void);
-extern void ust_after_setuid(void);
-extern void ust_after_setgid(void);
-extern void ust_after_seteuid(void);
-extern void ust_after_setegid(void);
-extern void ust_after_setreuid(void);
-extern void ust_after_setregid(void);
-extern void ust_after_setresuid(void);
-extern void ust_after_setresgid(void);
+// FIXME: This header should be renamed
+
+// This is ABI for liblttng-ust-fork
+
+
+// Owned by liblttng-ust.so, no extern
+void ust_before_fork(sigset_t *save_sigset);
+void ust_after_fork_parent(sigset_t *restore_sigset);
+void ust_after_fork_child(sigset_t *restore_sigset);
+void ust_after_setns(void);
+void ust_after_unshare(void);
+void ust_after_setuid(void);
+void ust_after_setgid(void);
+void ust_after_seteuid(void);
+void ust_after_setegid(void);
+void ust_after_setreuid(void);
+void ust_after_setregid(void);
+void ust_after_setresuid(void);
+void ust_after_setresgid(void);
 
 #ifdef __cplusplus
 }
